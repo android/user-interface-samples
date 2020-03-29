2a290c617f0915fc96093db6f7b4e0dcc4230370
Kenneth Ford <kennethford@google.com>
2020-03-28 23:36:08 -0700

    Moved all the view logic in SplitLayoutActivity back to a custom SplitLayout class

--- WindowManager/app/src/main/java/com/example/windowmanagersample/SplitLayoutActivity.kt
+++ WindowManager/app/src/main/java/com/example/windowmanagersample/SplitLayoutActivity.kt
@@ -18,18 +18,14 @@
 
 package com.example.windowmanagersample
 
-import android.graphics.Rect
 import android.os.Bundle
-import android.view.View
-import android.widget.FrameLayout
 import androidx.core.util.Consumer
 import androidx.core.view.doOnLayout
-import androidx.window.DisplayFeature
 import androidx.window.WindowLayoutInfo
 import androidx.window.WindowManager
 import com.example.windowmanagersample.databinding.ActivitySplitLayoutBinding
 
-/** Demo of an Activity splitting views across a [DeviceFeature]. */
+/** Demo of [SplitLayout]. */
 class SplitLayoutActivity : BaseSampleActivity() {
 
     private lateinit var windowManager: WindowManager
@@ -40,132 +36,11 @@ class SplitLayoutActivity : BaseSampleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplitLayoutBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
         windowManager = WindowManager(this, getTestBackend())
         binding.root.doOnLayout {
-            updateWindowLayout(windowManager.windowLayoutInfo)
+            binding.splitLayout.updateWindowLayout(windowManager.windowLayoutInfo)
         }
-    }
-
-    private fun updateWindowLayout(windowLayoutInfo: WindowLayoutInfo) {
-        val splitPositions = splitViewPositions(
-            binding.contentLayout.root,
-            binding.controlLayout.root,
-            windowLayoutInfo
-        )
-
-        if (splitPositions != null) {
-            val startPosition = splitPositions[0]
-            val startWidthSpec = View.MeasureSpec.makeMeasureSpec(
-                startPosition.width(),
-                View.MeasureSpec.EXACTLY
-            )
-            val startHeightSpec = View.MeasureSpec.makeMeasureSpec(
-                startPosition.height(),
-                View.MeasureSpec.EXACTLY
-            )
-            binding.contentLayout.root.measure(startWidthSpec, startHeightSpec)
-            binding.contentLayout.root.layout(
-                startPosition.left, startPosition.top, startPosition.right,
-                startPosition.bottom
-            )
-
-            val endPosition = splitPositions[1]
-            val endWidthSpec = View.MeasureSpec.makeMeasureSpec(
-                endPosition.width(),
-                View.MeasureSpec.EXACTLY
-            )
-            val endHeightSpec = View.MeasureSpec.makeMeasureSpec(
-                endPosition.height(),
-                View.MeasureSpec.EXACTLY
-            )
-            binding.controlLayout.root.measure(endWidthSpec, endHeightSpec)
-            binding.controlLayout.root.layout(
-                endPosition.left, endPosition.top, endPosition.right,
-                endPosition.bottom
-            )
-        }
-    }
-
-    /**
-     * Get the position of the split for this view.
-     * @return A rect that defines of split, or {@code null} if there is no split.
-     */
-    private fun splitViewPositions(
-        startView: View,
-        endView: View,
-        windowLayoutInfo: WindowLayoutInfo
-    ): Array<Rect>? {
-
-        // Calculate the area for view's content with padding
-        val paddedWidth = binding.root.width - binding.root.paddingLeft - binding.root.paddingRight
-        val paddedHeight =
-            binding.root.height - binding.root.paddingTop - binding.root.paddingBottom
-
-        for (feature in windowLayoutInfo.displayFeatures) {
-            // Only a hinge or a fold can split the area in two
-            if (feature.type != DisplayFeature.TYPE_FOLD && feature.type != DisplayFeature.TYPE_HINGE) {
-                continue
-            }
-
-            val splitRect = getFeatureBoundsInWindow(feature, binding.root) ?: continue
-
-            if (feature.bounds.left == 0) { // Horizontal layout
-                val topRect = Rect(
-                    startView.paddingLeft,
-                    startView.paddingTop,
-                    startView.paddingLeft + paddedWidth,
-                    splitRect.top
-                )
-                val bottomRect = Rect(
-                    endView.paddingLeft, splitRect.bottom,
-                    endView.paddingLeft + paddedWidth,
-                    endView.paddingTop + paddedHeight
-                )
-
-                if (measureAndCheckMinSize(topRect, startView) &&
-                    measureAndCheckMinSize(bottomRect, endView)
-                ) {
-                    return arrayOf(topRect, bottomRect)
-                }
-            } else if (feature.bounds.top == 0) { // Vertical layout
-                val leftRect = Rect(
-                    startView.paddingLeft,
-                    startView.paddingTop,
-                    splitRect.left,
-                    startView.paddingTop + paddedHeight
-                )
-                val rightRect = Rect(
-                    splitRect.right,
-                    endView.paddingTop,
-                    endView.paddingLeft + paddedWidth,
-                    endView.paddingTop + paddedHeight
-                )
-
-                if (measureAndCheckMinSize(leftRect, startView) &&
-                    measureAndCheckMinSize(rightRect, endView)
-                ) {
-                    return arrayOf(leftRect, rightRect)
-                }
-            }
-        }
-
-        return null
-    }
-
-    /**
-     * Measure a child view and see it if will fit in the provided rect.
-     * <p>Note: This method calls [View.measure] on the child view, which updates
-     * its stored values for measured with and height. If the view will end up with different
-     * values, it should be measured again.
-     */
-    private fun measureAndCheckMinSize(rect: Rect, childView: View): Boolean {
-        val widthSpec = View.MeasureSpec.makeMeasureSpec(rect.width(), View.MeasureSpec.AT_MOST)
-        val heightSpec = View.MeasureSpec.makeMeasureSpec(rect.height(), View.MeasureSpec.AT_MOST)
-        childView.measure(widthSpec, heightSpec)
-        return childView.measuredWidthAndState and FrameLayout.MEASURED_STATE_TOO_SMALL == 0 &&
-                childView.measuredHeightAndState and FrameLayout.MEASURED_STATE_TOO_SMALL == 0
     }
 
     override fun onAttachedToWindow() {
@@ -180,7 +55,7 @@ class SplitLayoutActivity : BaseSampleActivity() {
 
     inner class LayoutStateChangeCallback : Consumer<WindowLayoutInfo> {
         override fun accept(newLayoutInfo: WindowLayoutInfo) {
-            updateWindowLayout(newLayoutInfo)
+            binding.splitLayout.updateWindowLayout(newLayoutInfo)
         }
     }
 }
