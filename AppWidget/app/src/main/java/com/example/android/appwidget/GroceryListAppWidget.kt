b8e499e37fcdb15442c9d0e5db36f2b7268dbf06
Takeshi Hagikura <thagikura@google.com>
2021-04-07 19:59:51 +0900

    Add a AppWidget sample
    At this moment this sample includes widget with a grocery list with checkboxes
    that demonstrates:
    - Description in widget preview
    - Preview layout
    - Dynamic coloring
    - Rounded corners and padding
    
    Other APIs are going to be added in follow up CLs.
    It may be ideal to wire the grocery list with SQLite database, but it's
    not prioritized because that part is not relevant with the Widget APIs.
    (PS1 only adds an Empty Activity to make the review easier)
    
    Change-Id: Ic97986f1716221a6338599aad6f8cba43a80d0d7
    (cherry picked from commit 341d2bb29311b23e2a85564e4941f4cbba437f25)

---
+++ AppWidget/app/src/main/java/com/example/android/appwidget/GroceryListAppWidget.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.appwidget
+
+import android.annotation.SuppressLint
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import android.widget.RemoteViewsService
+
+/**
+ * Implementation of App Widget functionality for the grocery list.
+ */
+class GroceryListAppWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+@SuppressLint("RemoteViewLayout")
+private fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val views = RemoteViews(context.packageName, R.layout.widget_grocery_list)
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
