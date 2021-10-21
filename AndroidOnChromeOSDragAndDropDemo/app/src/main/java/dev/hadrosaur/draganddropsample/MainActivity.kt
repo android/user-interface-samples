04e8016139dc99c01e529d2e344573dca9cc5457
Emilie Roberts <hadrosaur@google.com>
2021-10-21 13:26:55 +0200

    Added copyright info

--- AndroidOnChromeOSDragAndDropDemo/app/src/main/java/dev/hadrosaur/draganddropsample/MainActivity.kt
+++ AndroidOnChromeOSDragAndDropDemo/app/src/main/java/dev/hadrosaur/draganddropsample/MainActivity.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dev.hadrosaur.draganddropsample
 
 import android.content.ClipData
@@ -16,7 +32,6 @@ import androidx.core.content.ContextCompat.getColor
 import dev.hadrosaur.draganddropsample.databinding.ActivityMainBinding
 import java.io.FileInputStream
 import java.io.FileNotFoundException
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
