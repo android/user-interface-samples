a6e83af9f03ec1b7fb3ac98b49e547c071ec7d35
Jason Mylen <jmylen@google.com>
2022-04-06 14:31:27 -0400

    Add initial home page of vibration effects.

--- Haptics/app/src/main/java/com/example/android/haptics/samples/ui/theme/Shape.kt
+++ Haptics/app/src/main/java/com/example/android/haptics/samples/ui/theme/Shape.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.example.android.haptics.samples.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,5 +22,8 @@ import androidx.compose.ui.unit.dp
 val Shapes = Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    large = RoundedCornerShape(10.dp),
 )
+
+val DrawerShape = RoundedCornerShape(0.dp, 16.dp, 16.dp, 0.dp)
+val DrawerButtonShape = RoundedCornerShape(50)
--- Haptics/app/src/main/java/com/example/android/haptics/samples/ui/theme/Type.kt
+++ Haptics/app/src/main/java/com/example/android/haptics/samples/ui/theme/Type.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,32 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.example.android.haptics.samples.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    defaultFontFamily = FontFamily.SansSerif,
 )
