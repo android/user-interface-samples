1d8635dd7cc5e05cf9f44339a0e18431407f8b6f
marcelpinto <marcelpinto@google.com>
2022-08-11 09:50:17 +0200

    Use URIs instead of bitmaps for ImageGlanceWidget.kt
    By using a FileProvider we can directly provide access to the image in the cache to the launcher.
    
    Note: this approach has some issues when the launcher is not the default or another process is hosting the widget instead.

--- AppWidget/app/src/main/java/com/example/android/appwidget/glance/image/ImageWorker.kt
+++ AppWidget/app/src/main/java/com/example/android/appwidget/glance/image/ImageWorker.kt
@@ -17,8 +17,13 @@
 package com.example.android.appwidget.glance.image
 
 import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION
+import android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION
+import android.content.pm.PackageManager
 import android.util.Log
 import androidx.compose.ui.unit.DpSize
+import androidx.core.content.FileProvider.getUriForFile
 import androidx.glance.GlanceId
 import androidx.glance.appwidget.GlanceAppWidgetManager
 import androidx.glance.appwidget.state.updateAppWidgetState
@@ -151,7 +156,31 @@ class ImageWorker(
 
         // Get the path of the loaded image from DiskCache.
         val path = context.imageLoader.diskCache?.get(url)?.use { snapshot ->
-            snapshot.data.toFile().path
+            val imageFile = snapshot.data.toFile()
+
+            // Use the FileProvider to create a content URI
+            val contentUri = getUriForFile(
+                context,
+                "com.example.android.appwidget.fileprovider",
+                imageFile
+            )
+
+            // Find the current launcher to grant permissions
+            val resolveInfo = context.packageManager.resolveActivity(
+                Intent(Intent.ACTION_MAIN).apply { addCategory(Intent.CATEGORY_HOME) },
+                PackageManager.MATCH_DEFAULT_ONLY
+            )
+            val launcherName = resolveInfo?.activityInfo?.packageName
+            if (launcherName != null) {
+                context.grantUriPermission(
+                    launcherName,
+                    contentUri,
+                    FLAG_GRANT_READ_URI_PERMISSION or FLAG_GRANT_PERSISTABLE_URI_PERMISSION
+                )
+            }
+
+            // return the path
+            contentUri.toString()
         }
         return requireNotNull(path) {
             "Couldn't find cached file"
