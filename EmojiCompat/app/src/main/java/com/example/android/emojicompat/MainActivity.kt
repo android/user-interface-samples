98da5956855223a3f800d58ca900f4289fb6a130
Alejandra Stamato <stamato@google.com>
2023-02-01 13:40:05 +0000

    Updating kt, gradle, dependencies

--- EmojiCompat/app/src/main/java/com/example/android/emojicompat/MainActivity.kt
+++ EmojiCompat/app/src/main/java/com/example/android/emojicompat/MainActivity.kt
@@ -27,13 +27,12 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         // [U+1F469] (WOMAN) + [U+200D] (ZERO WIDTH JOINER) + [U+1F4BB] (PERSONAL COMPUTER)
-        private val WOMAN_TECHNOLOGIST = "\uD83D\uDC69\u200D\uD83D\uDCBB"
+        private const val WOMAN_TECHNOLOGIST = "\uD83D\uDC69\u200D\uD83D\uDCBB"
 
         // [U+1F469] (WOMAN) + [U+200D] (ZERO WIDTH JOINER) + [U+1F3A4] (MICROPHONE)
-        private val WOMAN_SINGER = "\uD83D\uDC69\u200D\uD83C\uDFA4"
+        private const val WOMAN_SINGER = "\uD83D\uDC69\u200D\uD83C\uDFA4"
 
-        @JvmField
-        val EMOJI = WOMAN_TECHNOLOGIST + " " + WOMAN_SINGER
+        const val EMOJI = "$WOMAN_TECHNOLOGIST $WOMAN_SINGER"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
